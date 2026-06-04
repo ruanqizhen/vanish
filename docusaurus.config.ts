9ff2d9d08c8bbf0b433f3a3a883092ed8e3a70bd
ruanqizhen <ruanqizhen@gmail.com>
2026-06-03 20:00:39 -0400

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ruanqizhen/vanish/tree/main/',
+            'https://github.com/ruanqizhen/vanish/edit/main/',
         },
         blog: false,
         theme: {
